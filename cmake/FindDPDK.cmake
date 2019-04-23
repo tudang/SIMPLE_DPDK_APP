b6362c66e765ccf3529da6c664616d134b0381ee
Tu Dang <Tu.Dang@wdc.com>
2019-04-23 10:31:39 -0700

    Update build for DPDK 19.02

--- cmake/FindDPDK.cmake
+++ cmake/FindDPDK.cmake
@@ -13,10 +13,10 @@ find_path(DPDK_INCLUDE_DIR rte_config.h
 set(components
     acl bbdev bitratestats bpf bus_dpaa bus_fslmc bus_ifpga bus_pci bus_vdev cfgfile cmdline
      compressdev cryptodev distributor eal efd ethdev eventdev
-    flow_classify gro gso hash ip_frag jobstats kni kvargs latencystats
-    lpm mbuf member mempool mempool_bucket mempool_dpaa2 mempool_dpaa
+    flow_classify gro gso hash ip_frag jobstats kni kvargs latencystats stack
+    lpm mbuf member mempool mempool_bucket mempool_dpaa2 mempool_dpaa common_dpaax
     mempool_ring mempool_stack meter metrics net pci pdump pipeline pmd_af_packet
-    pmd_ark pmd_avf pmd_avp pmd_axgbe pmd_bbdev_null pmd_bnxt pmd_bond pmd_crypto_scheduler
+    pmd_ark pmd_avp pmd_axgbe pmd_bbdev_null pmd_bnxt pmd_bond pmd_crypto_scheduler
     pmd_cxgbe pmd_dpaa2 pmd_dpaa2_cmdif pmd_dpaa2_event pmd_dpaa2_qdma pmd_dpaa2_sec
     pmd_dpaa pmd_dpaa_event pmd_dpaa_sec pmd_e1000 pmd_ena pmd_enic pmd_failsafe pmd_fm10k
     pmd_i40e pmd_ifpga_rawdev pmd_ixgbe pmd_kni pmd_lio pmd_nfp pmd_null
